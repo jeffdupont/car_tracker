b1c6d079a5f96d3aa668c23f9f7f452e129b4e85
Jeff Dupont <jeff.dupont@gmail.com>
2015-01-14 20:36:34 -0700

    initial import

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        files: {
+          'public/css/app.css': 'public/scss/app.scss'
+        }
+      }
+    },
+
+    copy: {
+      modern: {
+        expand: true,
+        cwd: 'public/vendor/components/foundation/js/',
+        src: '**',
+        dest: 'public/dist/js/',
+        filter: 'isFile',
+      },
+      fonts: {
+        expand: true,
+        cwd: 'public/vendor/components/font-awsome/fonts/',
+        src: '**',
+        dest: 'public/dist/fonts/',
+        filter: 'isFile',
+      }
+    },
+
+    concat: {
+      css: {
+        src: [
+        'public/vendor/components/foundation/css/normalize.css',
+        'public/vendor/components/font-awsome/css/font-awesome.css',
+        'public/css/app.css',
+        ],
+        dest: 'public/dist/css/compiled.css'
+      },
+      js: {
+        src: [
+        'public/vendor/components/moment/moment.js',
+        'public/js/**/*.js',
+        'public/js/*.js',
+        ],
+        dest: 'public/dist/js/compiled.js'
+      }
+    },
+
+    cssmin: {
+      css: {
+        src: 'public/dist/css/compiled.css',
+        dest: 'public/dist/css/compiled.min.css'
+      }
+    },
+
+    uglify: {
+      js: {
+        files: {
+          'public/dist/js/compiled.min.js': ['public/dist/js/compiled.js']
+        }
+      },
+      options: {
+        mangle: false
+      }
+    },
+
+    watch: {
+      files: [ 'public/css/*', 'public/js/*', 'public/scss/*' ],
+      tasks: [ 'sass', 'concat:css', 'cssmin:css', 'concat:js', 'uglify:js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+
+  grunt.registerTask('default', [
+  'sass',
+  'copy:modern',
+  'copy:fonts',
+  'concat:css',
+  'cssmin:css',
+  'concat:js',
+  'uglify:js'
+  ]);
+};
---
+++ install.sh
@@ -0,0 +1,48 @@
+sudo apt-get update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+sudo apt-get install -y vim curl python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php5
+sudo apt-get update
+
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug redis-server
+
+cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+xdebug.scream=1
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+EOF
+
+sudo a2enmod rewrite
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+
+sudo service apache2 restart
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+cp /etc/redis/redis.conf /etc/redis/redis.conf.default
+cat << EOF | sudo tee /etc/redis/redis.conf
+daemonize yes
+pidfile /var/run/redis.pid
+logfile /var/log/redis/redis.log
+port 6379
+bind 127.0.0.1
+timeout 300
+loglevel notice
+## Default configuration options
+databases 16
+save 900 1
+save 300 10
+save 60 10000
+rdbcompression yes
+dbfilename dump.rdb
+appendonly no
+EOF
+
+sudo service redis-server restart
