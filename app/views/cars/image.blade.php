0ee28cf078300049af318f0f3ad09b8fef5903e4
Jeff Dupont <jeff.dupont@gmail.com>
2015-02-09 09:47:53 -0700

    fixed upload image from camera

--- app/views/cars/image.blade.php
+++ app/views/cars/image.blade.php
@@ -11,7 +11,7 @@
   <img src="{{ URL::route('cars.image', $car->id) }}"/>
 @endif
 </div>
-<input type="file" name="image" id="image" class="file-upload" accept="image/*" />
+<input type="file" name="image" id="image" class="file-upload" accept="image/*;capture=camera" />
 
 
 @section('script')
@@ -21,8 +21,32 @@
       script: 'upload',
       rules: {
         allowedFileTypes: 'image/jpeg,image/png,image/gif',
-        maxSize: 2500000
+        // maxSize: 2500000
       }
+    })
+    .on('lu:errors', function (e, errors) {
+      console.log(errors);
+      // var isErrors = false;
+      // $('#display').html('');
+      // $.each(errors, function (i, error) {
+      //   if (error.errors.length > 0) {
+      //     isErrors = true;
+      //     $.each(error.errors, function (i, errorInfo) {
+      //       $('#display').append('<br />ERROR! File: ' + error.name + ' - Info: ' + JSON.stringify(errorInfo));
+      //     });
+      //   }
+      // });
+      // if (!isErrors) {
+      //   $('#display').append('<br />No errors');
+      // }
+    })
+    .on('lu:before', function (e, files) {
+      console.log(files);
+      // $('#display').append('<br />Uploading ' + files.length + ' file(s)...');
+    })
+    .on('lu:progress', function (e, percentage) {
+      console.log(percentage);
+      // $('#display').append('<br />Progress ' + percentage + '%<br />');
     })
     .on('lu:success', function (e, response) {
       var img = $("#car_image img");
